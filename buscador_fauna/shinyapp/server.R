cde5d6cb548b8ebee09d42a61d021ff4265b9702
Maria Teresa Ortiz <teresa.ortiz.mancera@mail.com>
2016-01-21 11:10:17 -0600

    Change directories for the app to work in shiny server

--- buscador_fauna/shinyapp/server.R
+++ buscador_fauna/shinyapp/server.R
@@ -3,106 +3,101 @@ library(plyr)
 library(dplyr)
 library(tidyr)
 library(shiny)
-library(rgdal)
+# library(rgdal)
 library(DT)
 library(DBI)
-library(leaflet)
+library(querysnmb)
 
+# load("../datos/malla.Rdata")
 load("/Volumes/ARCHIVOS_C/Mac_Pro/SNMB/datos/malla.Rdata")
-
 # Filtramos los conglomerados en la base para hacer el mapa de conglomerados
 # muestreados en leaflet, tiene que ser aquí para que no sea reactiva y sólo se
 # calcule una vez
 PASS_SNMB = Sys.getenv("PASS_SNMB")
-base_input_aux <- src_postgres(dbname = "snmb", host = "dbms", user = "snmb", 
+base_input <- src_postgres(dbname = "snmb", host = "dbms", user = "snmb",
   password = PASS_SNMB)
-conglomerado_ids <- tbl(base_input_aux, "conglomerado_muestra") %>%
+conglomerado_ids <- tbl(base_input, "conglomerado_muestra") %>%
   collect() %>%
   mutate(id = as.numeric(nombre)) %>%
   `$`("id")
-RPostgreSQL::dbDisconnect(base_input_aux$con)  
 
-malla <- malla %>% 
-  filter(cgl %in% conglomerado_ids) %>% 
+malla <- malla %>%
+  filter(cgl %in% conglomerado_ids) %>%
   select(cgl_id = cgl, lng = lon, lat)
-
-source("/Volumes/ARCHIVOS_C/Mac_Pro/SNMB/queryfactory_R_snmb/species.R", 
-  local = TRUE)
-source("/Volumes/ARCHIVOS_C/Mac_Pro/SNMB/queryfactory_R_snmb/querySpecies.R", 
-  local = TRUE)
 
 shinyServer(function(input, output){
   # datos reactivos
-  animal_mult <- eventReactive(input$actualiza, {  
-    speciesFinder(input$especies, malla)
+  animal_mult <- eventReactive(input$actualiza, {
+    querySpPresence(database = base_input, noms = input$especies,
+      malla)
   })
   # mapa base con los conglomerados en azúl
   output$mapa_base <- renderLeaflet({
-    leaflet() %>% 
+    leaflet() %>%
       addTiles(options =  providerTileOptions(opacity = 0.75)) %>%
       fitBounds(lng1 = -117.82, lat1 = 32.64, lng2 = -84.91, lat2 = 13.39) %>%
       addCircleMarkers(data = malla,
-        lng =~lng, lat=~lat, 
-        popup=~paste("cgl:", cgl_id), 
+        lng =~lng, lat=~lat,
+        popup=~paste("cgl:", cgl_id),
         stroke = FALSE,
-        fillOpacity = 0.4, 
+        fillOpacity = 0.4,
         radius = 2,
         color = ~"navy",
         group = "base")
   })
-  
-  # graficar puntos sobre el mapa base que indican los conglomerados donde se 
+
+  # graficar puntos sobre el mapa base que indican los conglomerados donde se
   # detectó la especie(s) buscada(s), se indican con puntos rojos
   observeEvent(input$actualiza, {
     animales <- animal_mult()[[2]]
-    
-    leafletProxy("mapa_base", data = animales) %>% 
+
+    leafletProxy("mapa_base", data = animales) %>%
       # clearCircleMarkers() %>%
       clearGroup(group = "obs") %>%
       clearGroup(group = "highlights") %>%
       addCircleMarkers(
-        lng =~lng, lat=~lat, 
-        popup=~paste("cgl:", cgl_id), 
+        lng =~lng, lat=~lat,
+        popup=~paste("cgl:", cgl_id),
         stroke = FALSE,
-        fillOpacity = 0.8, 
+        fillOpacity = 0.8,
         radius = 4,
         color = ~"red",
         group = "obs")
   })
-  
-  # agregar marcador en mapa que indica los renglones de la tabla seleccionados 
+
+  # agregar marcador en mapa que indica los renglones de la tabla seleccionados
   # con el mouse
   observeEvent(input$tbl_rows_selected, {
     animales <- animal_mult()[[2]]
-    
+
     animales_sub <- animales[as.numeric(input$tbl_rows_selected), drop = FALSE]
-    
-    leafletProxy("mapa_base", data = animales_sub) %>% 
+
+    leafletProxy("mapa_base", data = animales_sub) %>%
       clearGroup(group = "highlights") %>%
       addCircleMarkers(
-        lng =~lng, lat=~lat, 
-        popup =~paste("cgl:", cgl_id), 
+        lng =~lng, lat=~lat,
+        popup =~paste("cgl:", cgl_id),
         #stroke = FALSE,
-        #fillOpacity = 1, 
+        #fillOpacity = 1,
         radius = 8,
         weight = 2,
         #color = ~"red",
         group = "highlights")
   })
-  
-  # tabla con información de las especies buscadas, la tabla es a nivel 
+
+  # tabla con información de las especies buscadas, la tabla es a nivel
   # conglomerado
   output$tbl <- DT::renderDataTable({
-    tab <- animal_mult()[[1]] 
-  }, 
+    tab <- animal_mult()[[1]]
+  },
     options = list(
-      lengthMenu = c(5, 10, 15, 20, -1), 
-      pageLength = 15, 
+      lengthMenu = c(5, 10, 15, 20, -1),
+      pageLength = 15,
       columnDefs = list(
         list(className = 'dt-center', targets = c(5, 6, 7, 8, 9, 10)))
     )
   )
-  
+
   # descarga de la tabla a nivel conglomerado
   output$downloadCgl <- downloadHandler(
     filename = "tab_cgls.csv",
@@ -110,19 +105,19 @@ shinyServer(function(input, output){
       write.csv(animal_mult()[[1]], file)
     }
   )
-  
+
   # construcción de tablas a nivel archivo
-  dataFiles <- reactive({  
+  dataFiles <- reactive({
     queryTabs <- switch(input$tab,
       ei = queryInvaders,
       he = queryFootprints,
-      er = querySpecimens,
-      camara = queryInvaders,
+      er = querySpecimen,
+      camara = queryFiles,
       queryInvaders)
     noms <- ifelse(input$sel_especies == "especies_noms", input$especies, "all")
-    queryTabs(noms = noms)
+    queryTabs(database = base_input, noms = noms)
   })
-  
+
   # descarga de la tabla a nivel archivo
   output$downloadFiles <- downloadHandler(
     filename = "tab_archivos.csv",
@@ -130,5 +125,5 @@ shinyServer(function(input, output){
       write.csv(dataFiles(), file)
     }
   )
-  
+
 })
--- buscador_fauna/shinyapp/ui.R
+++ buscador_fauna/shinyapp/ui.R
@@ -1,4 +1,3 @@
-library(shiny)
 library(leaflet)
 # Define UI for miles per gallon application
 shinyUI(fluidPage(
@@ -8,38 +7,39 @@ shinyUI(fluidPage(
 
   wellPanel(
     textInput("especies", label = "Nombre:", value = "vaca|taurus|caballo|equus"),
-    helpText("Ingresa el nombre o nombres (común y/o científico) de los animales 
-      o plantas que deseas buscar, los nombres deben estar separados por el 
-      símbolo '|'. Ejemplo: vaca|taurus|caballo|equus"),    
+    helpText("Ingresa el nombre o nombres (común y/o científico) de los animales
+      o plantas que deseas buscar, los nombres deben estar separados por el
+      símbolo '|'. Ejemplo: vaca|taurus|caballo|equus"),
     actionButton("actualiza", "actualiza", icon("refresh"))
-    
+
   ),
 #   mainPanel(
 #     p(),
-#     leafletOutput("mapa_base", width = "800px", height = "500px"), 
+#     leafletOutput("mapa_base", width = "800px", height = "500px"),
 #     p(),
 #     dataTableOutput("tbl"),
 #     downloadButton('downloadCgl', 'Descargar tabla conglomerados')
 #   )
   mainPanel(
     tabsetPanel(
-      tabPanel("Mapa", 
-        leafletOutput("mapa_base", width = "800px", height = "500px"), 
+      tabPanel("Mapa",
+        leafletOutput("mapa_base", width = "800px", height = "500px"),
         p(),
-        dataTableOutput("tbl")
+        dataTableOutput("tbl"),
+        p()
       ),
-      tabPanel("Descargas", 
-        h3("Descarga de tablas en formato csv"), 
+      tabPanel("Descargas",
+        h3("Descarga de tablas en formato csv"),
         wellPanel(
-#         p("El siguiente botón descarga la tabla de la pestaña Mapa en formato 
+#         p("El siguiente botón descarga la tabla de la pestaña Mapa en formato
 #           csv."),
         h4("Tabla de presencia a nivel conglomerado"),
-          p("Para descargar esta tabla es necesario haber ejecutado una búsqueda 
-            de especie(s), es la tabla análoga a la que aparece en la parte 
+          p("Para descargar esta tabla es necesario haber ejecutado una búsqueda
+            de especie(s), es la tabla análoga a la que aparece en la parte
             inferior de la pestaña Mapa."),
-        downloadButton('downloadCgl', 'Presencia conglomerados'), 
-        h4("Tablas con rutas a archivos"), 
-        p("Las siguientes tablas se descargan por tipo de registro (especie 
+        downloadButton('downloadCgl', 'Presencia conglomerados'),
+        h4("Tablas con rutas a archivos"),
+        p("Las siguientes tablas se descargan por tipo de registro (especie
           invasora, huellas/excretas, trampa cámara)."),
         radioButtons("tab", "Tabla",
           list("Especies Invasoras" = "ei",
